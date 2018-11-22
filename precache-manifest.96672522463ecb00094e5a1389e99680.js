722e7ce98a0b5792809912b5775914cf049b68df
Andy Mantell <134642+andymantell@users.noreply.github.com>
2018-11-22 15:39:49 +0000

    Updates

---
+++ precache-manifest.96672522463ecb00094e5a1389e99680.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "765f527c5211d29f2808",
+    "url": "/worst-form-control-ever/static/js/main.765f527c.chunk.js"
+  },
+  {
+    "revision": "5f7ce33a918f82104582",
+    "url": "/worst-form-control-ever/static/js/1.5f7ce33a.chunk.js"
+  },
+  {
+    "revision": "ef11042880d270e8a7db",
+    "url": "/worst-form-control-ever/static/js/runtime~main.ef110428.js"
+  },
+  {
+    "revision": "137f0c52dadab4da98870d0d9d3e0b5d",
+    "url": "/worst-form-control-ever/index.html"
+  }
+];
